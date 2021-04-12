6947b125411f7d19931eacef188194ff2b0a6486
Paweł Kierat <kierat.pawel@gmail.com>
2021-04-12 14:24:24 +0200

    Updates

---
+++ precache-manifest.98a06fc263d6ded1edcae2c524779bb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8047971dd15bfd9e2d7",
+    "url": "/lotto_picker_react/static/css/main.2e48cf7f.chunk.css"
+  },
+  {
+    "revision": "f8047971dd15bfd9e2d7",
+    "url": "/lotto_picker_react/static/js/main.a5077c3e.chunk.js"
+  },
+  {
+    "revision": "796dbc355e7e03df77e6",
+    "url": "/lotto_picker_react/static/js/runtime~main.7b7ce953.js"
+  },
+  {
+    "revision": "1075c91bfba69aed23a5",
+    "url": "/lotto_picker_react/static/js/2.3dcc18fc.chunk.js"
+  },
+  {
+    "revision": "134dd28fd1b6456543bca93929075d03",
+    "url": "/lotto_picker_react/static/media/baner.134dd28f.jpg"
+  },
+  {
+    "revision": "52c3fd88df635d6ffdcc8e31bc61bdae",
+    "url": "/lotto_picker_react/index.html"
+  }
+];
